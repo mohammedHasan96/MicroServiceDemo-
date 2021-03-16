2a10afa9ecc24e7c3983b9f953de82105b1f7ee9
developer.ajory <developer.ajory@gmail.com>
2021-03-16 22:13:29 +0200

    Added Wrapper to response

---
+++ MicroServicesDemo.Basket/Models/Wrapper.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServicesDemo.Basket.Models
+{
+    public class Wrapper<T>
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+        public static implicit operator Wrapper<T>(T t) => new Wrapper<T> { Success = true, Data = t };
+    }
+}
